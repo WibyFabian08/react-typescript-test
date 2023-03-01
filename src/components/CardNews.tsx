040774dd3ead8e73524edef4b3710c7f8057b823
wibyFabian08 <wibyfabian08@gmail.com>
2023-03-01 13:31:20 +0700

    refacor

---
+++ src/components/CardNews.tsx
@@ -0,0 +1,36 @@
+import { ArticleType } from "../services/data-types";
+import {
+  Card,
+  Button,
+} from "antd";
+
+import { Link } from "react-router-dom";
+
+const { Meta } = Card;
+
+const CardNews = (props: { item: ArticleType }) => {
+  return (
+    <Card
+      cover={
+        <img
+          alt="example"
+          src={
+            props.item.urlToImage ||
+            "https://leaveitwithme.com.au/wp-content/uploads/2013/11/dummy-image-square.jpg"
+          }
+        />
+      }
+      actions={[
+        <Link style={{ padding: "5px" }} to={props.item.url} target="_blank">
+          <Button size="large" style={{ float: "right" }}>
+            Detail News
+          </Button>
+        </Link>,
+      ]}
+    >
+      <Meta title={props.item.author} description={props.item.title} />
+    </Card>
+  );
+};
+
+export default CardNews;
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,121 +1,118 @@
-import React, { useCallback, useEffect, useState } from 'react';
-import { Layout, Menu, theme, List, Card, Skeleton, Button } from 'antd';
-import type { MenuProps } from 'antd';
+import React, { useCallback, useEffect, useState } from "react";
+import {
+  Layout,
+  Menu,
+  theme,
+  List,
+  Skeleton,
+  Typography,
+} from "antd";
+import type { MenuProps } from "antd";
 
-import navMenu from '../constants/navMenu';
-import { getNews } from '../api';
-import { ArticleType } from '../services/data-types';
+import navMenu from "../constants/navMenu";
+import { getNews } from "../api";
+import { ArticleType } from "../services/data-types";
+import CardNews from "../components/CardNews";
 
 const { Header, Content, Footer } = Layout;
-const { Meta } = Card;
+const { Title } = Typography;
 
 const Home: React.FC = () => {
-    const [isLoading, setIsLoading] = useState(false)
-    const [articles, setArticles] = useState([]);
-    const [category, setCategory] = useState<string>("sport")
-    const {
-        token: { colorBgContainer },
-    } = theme.useToken();
+  const [isLoading, setIsLoading] = useState(false);
+  const [articles, setArticles] = useState([]);
+  const [category, setCategory] = useState<string>("sport");
+  const [loadingTemp] = useState(new Array(12).fill(2));
+  
+  const {
+    token: { colorBgContainer },
+  } = theme.useToken();
 
-    const items: MenuProps["items"] = navMenu.map((value, _) => {
-        return {
-            key: `${value.value}`,
-            label: `${value.name}`
-        }
-    });
-
-    const onClick: MenuProps['onClick'] = (e) => {
-        setCategory(e.key)
+  const items: MenuProps["items"] = navMenu.map((value, _) => {
+    return {
+      key: `${value.value}`,
+      label: `${value.name}`,
     };
+  });
 
-    const getData = useCallback(() => {
-        setIsLoading(true)
-        getNews(category).then((res) => {
-            setArticles(res.data.articles)
-            setIsLoading(false)
-        })
-    }, [category])
+  const onClick: MenuProps["onClick"] = (e) => {
+    setCategory(e.key);
+  };
 
-    useEffect(() => {
-        getData()
-    }, [category, getData])
+  const getData = useCallback(() => {
+    setIsLoading(true);
+    getNews(category).then((res) => {
+      setArticles(res.data.articles);
+      setIsLoading(false);
+    });
+  }, [category]);
 
-    return (
-        <Layout className="layout">
-            <Header>
-                <Menu
-                    theme="dark"
-                    onClick={onClick}
-                    mode="horizontal"
-                    defaultSelectedKeys={[navMenu[0].value]}
-                    items={items}
-                />
-            </Header>
-            <Content style={{ padding: '0 50px' }}>
-                <div style={{ background: colorBgContainer, minHeight: "80vh", padding: "24px" }}>
-                    {
-                        isLoading ? <List grid={{
-                            gutter: 16,
-                            xs: 1,
-                            sm: 2,
-                            md: 3,
-                            lg: 4,
-                            xl: 4,
-                            xxl: 3,
-                        }}
-                            dataSource={navMenu}
-                            renderItem={_ => (
-                                <List.Item>
-                                    <Skeleton active />
-                                </List.Item>
+  useEffect(() => {
+    getData();
+  }, [category, getData]);
 
-                            )}
-                        >
-                        </List> : <List
-                            grid={{
-                                gutter: 16,
-                                xs: 1,
-                                sm: 2,
-                                md: 3,
-                                lg: 4,
-                                xl: 4,
-                                xxl: 3,
-                            }}
-                            dataSource={articles}
-                            renderItem={(item: ArticleType) => (
-                                <List.Item>
-                                    <Card
-                                        cover={
-                                            <img
-                                                alt="example"
-                                                src={item.urlToImage || "https://leaveitwithme.com.au/wp-content/uploads/2013/11/dummy-image-square.jpg"}
-                                            />
-                                        }
-                                        actions={[
-                                            <Button>Detail</Button>
-                                            //   <SettingOutlined key="setting" />,
-                                            //   <EditOutlined key="edit" />,
-                                            //   <EllipsisOutlined key="ellipsis" />,
-                                        ]}
-                                    >
-                                        <Meta
-                                            title={item.author}
-                                            description={item.title}
-                                        />
-                                    </Card>
-                                </List.Item>
-                            )}
-                        />
-                    }
-
-
-
-
-                </div>
-            </Content>
-            <Footer style={{ textAlign: 'center' }}>News App</Footer>
-        </Layout>
-    );
+  return (
+    <Layout className="layout">
+      <Header>
+        <Menu
+          theme="dark"
+          onClick={onClick}
+          mode="horizontal"
+          defaultSelectedKeys={[navMenu[0].value]}
+          items={items}
+        />
+      </Header>
+      <Content style={{ padding: "0 50px" }}>
+        <div
+          style={{
+            background: colorBgContainer,
+            minHeight: "80vh",
+            padding: "24px",
+          }}
+        >
+          {isLoading ? (
+            <List
+              grid={{
+                gutter: 16,
+                xs: 1,
+                sm: 2,
+                md: 3,
+                lg: 4,
+                xl: 4,
+                xxl: 3,
+              }}
+              dataSource={loadingTemp}
+              renderItem={(_) => (
+                <List.Item>
+                  <Skeleton active />
+                </List.Item>
+              )}
+            ></List>
+          ) : (
+            <List
+              grid={{
+                gutter: 16,
+                xs: 1,
+                sm: 2,
+                md: 3,
+                lg: 4,
+                xl: 4,
+                xxl: 3,
+              }}
+              dataSource={articles}
+              renderItem={(item: ArticleType) => (
+                <List.Item>
+                    <CardNews item={item}></CardNews>
+                </List.Item>
+              )}
+            />
+          )}
+        </div>
+      </Content>
+      <Footer style={{ textAlign: "center" }}>
+        <Title>React Test</Title>
+      </Footer>
+    </Layout>
+  );
 };
 
 export default Home;
